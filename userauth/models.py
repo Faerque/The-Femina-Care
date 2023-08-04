8d43e1b9878740f14b16465b50b8f71b7891c9f3
Muhammad Omar Faruk <202001512@eastdelta.edu.bd>
2023-08-04 12:18:56 +0600

    inital backend added

---
+++ userauth/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+# Create your models here.
+
+
+# User Account Manager
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError('User must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save()
+
+        return user
+
+# User Model Account
+
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255,)
+    is_active = models.BooleanField(default=True)
+    is_user = models.BooleanField(default=False)
+    is_Admin = models.BooleanField(default=True)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
